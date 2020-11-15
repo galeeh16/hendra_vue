7c55b1c0acd96d77a24939a419aae2ab08d95d23
Galih Anggoro Jati <anggorogalih8@gmail.com>
2020-11-15 12:32:30 +0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,53 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<b-container fluid>
+  <b-row class="my-3">
+    <div id="app">
+    <b-form-input v-model="text" placeholder="Enter your name">Input Name</b-form-input>
+    <b-button variant="danger"> Submit</b-button>
+   <button class="btn btn-danger" @click="getDataDb">{{test}}</button>
   </div>
+  <div>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Nama</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="user in users" v-bind:key="user.id">
+          <td>{{ user.name }}</td>
+          <td>{{ user.email }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  </b-row>
+</b-container>
+  
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios"
 
 export default {
   name: 'App',
+  data: function() {
+    return {
+      test: "Hendra Arya",
+      users: []
+    }
+  },
+  methods: {
+    async getDataDb() {
+      let response = await axios.get("http://localhost:8000/api/hendra"); //untuk connect ke Backend Laravel menggunakan API yang telah dicreate di dalam Laravel
+      let data = response.data;
+      this.users = data;
+      console.log(data);
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
